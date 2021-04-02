129942acce6fe3495ebe81dea1db9749c175de92
Sam Levy <slevy239@gmail.com>
2021-04-01 23:44:13 -0400

    functions

--- api_calls.py
+++ api_calls.py
@@ -8,24 +8,33 @@ uni = 'uniswap'
 link = 'chainlink'
 coins = [btc, eth, uni, link]
 
-# While loop iterating through the coin list
-i = 0
-while i < len(coins):
-    # prints all values in list
-    print(cg.get_price(ids=coins[i], vs_currencies='usd'))
-    i += 1
+
+def print_list():
+    # While loop iterating through the coin list
+    i = 0
+    while i < len(coins):
+        # prints all values in list
+        print(cg.get_price(ids=coins[i], vs_currencies='usd'))
+        i += 1
+    user_input()
 
 
-# allows for user input
-while input("Would you like to continue?\n") == 'yes':
-    new = input('Enter a value\n')
-    # check if values exists in the list
-    if new in coins:
-        print('choose a new value')
-    # appends list and prints updated list
-    else:
-        coins.append(new)
-        k = 0
-        while k < len(coins):
-            print(cg.get_price(ids=coins[k], vs_currencies='usd'))
-            k += 1
+def user_input():
+    # allows for user input
+    while input("Would you like to continue?\n") == 'yes':
+        new = input('Enter a value\n')
+
+        # check if values exists in the list
+        if new in coins:
+            print('Already exists in the list')
+            user_input()
+        # appends list and prints updated list
+        else:
+            coins.append(new)
+            k = 0
+            while k < len(coins):
+                print(cg.get_price(ids=coins[k], vs_currencies='usd'))
+                k += 1
+
+
+print_list()
