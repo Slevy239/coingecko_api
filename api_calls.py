ab78794dafe98c5b86bd3c86e79943df5641c163
Sam Levy <slevy239@gmail.com>
2021-04-01 23:26:41 -0400

    input loop

--- api_calls.py
+++ api_calls.py
@@ -1,4 +1,5 @@
 from pycoingecko import CoinGeckoAPI
+
 cg = CoinGeckoAPI()
 
 btc = 'bitcoin'
@@ -7,9 +8,24 @@ uni = 'uniswap'
 link = 'chainlink'
 coins = [btc, eth, uni, link]
 
-print(cg.get_price(ids=coins[1], vs_currencies='usd'))
+# While loop iterating through the coin list
+i = 0
+while i < len(coins):
+    # prints all values in list
+    print(cg.get_price(ids=coins[i], vs_currencies='usd'))
+    i += 1
 
-# i = 0
-# while i < len(coins):
-#     print(cg.get_price(ids=coins[i], vs_currencies='usd'))
-#     i += 1
+
+# allows for user input
+while input("Would you like to continue?\n") == 'yes':
+    new = input('Enter a value\n')
+    # check if values exists in the list
+    if new in coins:
+        print('choose a new value')
+    # appends list and prints updated list
+    else:
+        coins.append(new)
+        k = 0
+        while k < len(coins):
+            print(cg.get_price(ids=coins[k], vs_currencies='usd'))
+            k += 1
