5c6b73f72f3ad4b1dfdca9372723532224c4e38e
Sam Levy <slevy239@gmail.com>
2021-04-09 10:46:37 -0400

    new addresses

--- sushiVesting.py
+++ sushiVesting.py
@@ -1,7 +1,8 @@
 import re, json, requests
 import config
-# Potential Sushiswap farm rewards
 
+
+# Potential Sushiswap farm rewards
 
 url = 'https://raw.githubusercontent.com/sushiswap/sushi-vesting/master/amounts-10959148-12171394.json'
 merkle_1 = 'https://raw.githubusercontent.com/sushiswap/sushi-vesting/master/merkle/week-01/merkle-10959148-11003985.json'
@@ -11,11 +12,17 @@ resp = requests.get(url)
 data = json.loads(resp.text)
 
 mine = config.address
+josh = config.sutker
+coop = config.coop
 address = []
 
 if mine in data:
     address.append(mine)
-if mine in data2:
-    address.append(mine)
+    address.append(josh)
+    address.append(coop)
+# if mine in data2:
+    # address.append(mine)
+    # address.append(mine)
 
-print(address, 'amounts?')
+
+print(address, 'addresses?')
